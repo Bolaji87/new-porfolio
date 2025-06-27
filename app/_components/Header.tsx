a5ad3b4921726522ed80348b21b0a9527d056cb4
Bolaji87 <sakabolaji77@gmail.com>
2025-06-27 12:15:33 +0100

    remove all unused

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
   const { isOpen } = useNavModal();
 
   return (
-    <header className="py-4 bg-gray-50 w-full fixed scroll-smooth top-0 z-50 shadow-lg border-b border-b-gray-200 text-stone-700">
+    <header className="py-4 bg-gray-50 w-full fixed scroll-smooth top-0 z-50 shadow-lg border-b border-b-gray-200 ">
       <div className="flex text-lg  items-center max-w-6xl mx-auto justify-between lg:px-0 px-3">
         <h1 className="sm:text-xl text-lg  cursor-pointer font-semibold text-sky-500 hover:text-sky-600">
           SB
@@ -43,9 +43,6 @@ function Header() {
           <div className=" absolute top-20 right-2 ">
             {isOpen && <NavModal />}
           </div>
-          {/* <div className="sm:block hidden">
-            <ThemeToggle />
-          </div> */}
         </div>
       </div>
     </header>
